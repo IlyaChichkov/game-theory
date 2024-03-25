9c62e31579e877cad3759ad108e9ce355a03e06c
IlyaChichkov <ilya555chich@yandex.ru>
2024-03-25 10:48:04 +0300

    feat: initialize game match class

---
+++ src/game_match.h
@@ -0,0 +1,12 @@
+#ifndef GAME_THEORY_GAME_MATCH_H
+#define GAME_THEORY_GAME_MATCH_H
+
+class GameMatch {
+    /*
+     *  Игровой матч состоящий из 14 ходов
+     *  Содержит всю информацию о текущей игре, командах
+     */
+};
+
+
+#endif //GAME_THEORY_GAME_MATCH_H
