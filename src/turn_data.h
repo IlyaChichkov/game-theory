ed381592f470cbcaf5cda8015835e24946067059
IlyaChichkov <ilya555chich@yandex.ru>
2024-03-25 16:26:47 +0300

    feat: initialize turn data class

---
+++ src/turn_data.h
@@ -0,0 +1,14 @@
+//
+// Created by IlyaChichkov on 25.03.2024.
+//
+
+#ifndef GAME_THEORY_TURN_DATA_H
+#define GAME_THEORY_TURN_DATA_H
+
+
+class TurnData {
+
+};
+
+
+#endif //GAME_THEORY_TURN_DATA_H
