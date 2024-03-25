b35f7364a14bb7f99db289b8094113826c4cc54f
IlyaChichkov <ilya555chich@yandex.ru>
2024-03-25 16:28:32 +0300

    feat: implement game match methods

--- src/game_match.h
+++ src/game_match.h
@@ -31,6 +31,57 @@ class GameMatch {
      *  Игровой матч состоящий из 14 ходов
      *  Содержит всю информацию о текущей игре, командах
      */
+private:
+    std::vector<Team*> teams;
+    std::vector<Team*> strikeTeams;
+
+    void setup_teams() {
+        DefaultMatchActions matchActions;
+
+        for (int i = 0; i < 4; ++i) {
+            MyTeam* team = new MyTeam();
+            team->set_turn_actions(matchActions.Create());
+            teams.push_back(team);
+        }
+    }
+
+    void complete_turn() {
+        TurnData turnData;
+
+        for(const auto& team : this->teams)
+        {
+            ITurnAction selectedAction = team->make_turn(turnData);
+        }
+
+        compute_turn_results();
+    }
+
+    void compute_turn_results() {
+
+    }
+
+public:
+
+    void start() {
+        setup_teams();
+        for (int i = 0; i < 5; ++i) {
+            complete_turn();
+        }
+    }
+
+    void add_strike(Team *strikeTeam) {
+        this->strikeTeams.push_back(strikeTeam);
+    }
+
+    ~GameMatch() {
+        for (auto* team : teams) {
+            delete team;
+        }
+
+        for (auto* team : strikeTeams) {
+            delete team;
+        }
+    }
 };
 
 
