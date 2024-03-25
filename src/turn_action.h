7fc56930f4d9809c71116aa81a0fde28502eff79
IlyaChichkov <ilya555chich@yandex.ru>
2024-03-25 16:28:00 +0300

    feat: implement main turn actions

--- src/turn_action.h
+++ src/turn_action.h
@@ -1,13 +1,51 @@
 #ifndef GAME_THEORY_TURN_ACTION_H
 #define GAME_THEORY_TURN_ACTION_H
 
+#include <utility>
+#include "game_match.h"
+#include "team.h"
 
-class TurnAction {
+class GameMatch;
+class Team;
+
+enum class TurnActionType {
+    None,
+    ProductionChange,
+    Strike
+};
+
+class ITurnAction {
     /*
      *  Игровые действия выполняемые командами каждый ход
      *  (изменение продукции, забастовки)
      */
+public:
+    TurnActionType actionType = TurnActionType::None;
+    virtual ~ITurnAction() {}
 };
 
+class ProductionChange : public ITurnAction {
+private:
+    int delta = 0;
+public:
+    explicit ProductionChange(int _delta) {
+        actionType = TurnActionType::ProductionChange;
+        delta = _delta;
+    }
+};
+
+class ProvokeStrike : public ITurnAction {
+private:
+    Team *strikeTarget = nullptr;
+public:
+    explicit ProvokeStrike() {
+        actionType = TurnActionType::Strike;
+        strikeTarget = nullptr;
+    }
+
+    void SetTarget(Team *target) {
+        strikeTarget = target;
+    }
+};
 
 #endif //GAME_THEORY_TURN_ACTION_H
