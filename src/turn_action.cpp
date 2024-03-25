d88487ec610b0dc85dfefe37a73e0e02b4820b78
IlyaChichkov <ilya555chich@yandex.ru>
2024-03-25 10:48:30 +0300

    feat: initialize turn action class

---
+++ src/turn_action.cpp
@@ -0,0 +1,1 @@
+#include "turn_action.h"
---
+++ src/turn_action.h
@@ -0,0 +1,13 @@
+#ifndef GAME_THEORY_TURN_ACTION_H
+#define GAME_THEORY_TURN_ACTION_H
+
+
+class TurnAction {
+    /*
+     *  Игровые действия выполняемые командами каждый ход
+     *  (изменение продукции, забастовки)
+     */
+};
+
+
+#endif //GAME_THEORY_TURN_ACTION_H
