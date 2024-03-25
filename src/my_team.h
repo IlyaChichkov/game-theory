47e610d4abbf9912862a18a521ec74fd9eeb8da3
IlyaChichkov <ilya555chich@yandex.ru>
2024-03-25 17:16:35 +0300

    fix: switch turn actions list to shared ptr

--- src/my_team.h
+++ src/my_team.h
@@ -6,11 +6,27 @@
 #include "team.h"
 
 class MyTeam : public Team {
+private:
+    void turn()
+    {
+
+        if (!this->turn_actions.empty()) {
+            turn_actions.erase(turn_actions.begin());
+        }
+    }
 public:
-    ITurnAction make_turn(TurnData match) const override {
+    void set_prod(int val) {
+        production = val;
+    }
+
+    ITurnAction make_turn(TurnData match) override {
         std::cout << "Making turn..." << std::endl;
 
         ITurnAction& action = *turn_actions.at(0);
+
+        if (!this->turn_actions.empty()) {
+            turn_actions.erase(turn_actions.begin());
+        }
 
         if(action.actionType == TurnActionType::None)
         {
@@ -29,9 +45,7 @@ public:
     }
 
     ~MyTeam() {
-        for (ITurnAction* action : turn_actions) {
-            delete action;
-        }
+
     }
 };
 
