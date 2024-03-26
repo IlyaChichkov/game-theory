7e35e60fcc0cd4e71ba071f34bb1899177fd98ef
IlyaChichkov <ilya555chich@yandex.ru>
2024-03-26 15:07:13 +0300

    feat: change my_team strategy to random

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <ctime>
 #include "src/game_match.h"
 
 int main() {
+    srand(time(0));
+
     std::cout << "Game theory." << std::endl;
     GameMatch match;
     match.start();
