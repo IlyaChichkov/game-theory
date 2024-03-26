39366a1049fd8d16f7c6fa01d5b4b9973e0dd010
IlyaChichkov <ilya555chich@yandex.ru>
2024-03-26 11:59:58 +0300

    feat: add get_opponents method for turn data

--- src/turn_data.cpp
+++ src/turn_data.cpp
@@ -4,3 +4,13 @@
 
 #include "turn_data.h"
 #include "team.h"
+
+std::vector<std::shared_ptr<Team>> TurnData::get_opponents(int team_id) {
+    std::vector<std::shared_ptr<Team>> opponents;
+    for (const auto team : teams) {
+        if(team->ID() != team_id){
+            opponents.push_back(team);
+        }
+    }
+    return opponents;
+}
