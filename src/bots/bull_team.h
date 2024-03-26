9ef6f8ffabe43607be510a5ef1087e2a7864f45f
IlyaChichkov <ilya555chich@yandex.ru>
2024-03-26 13:25:14 +0300

    feat: added primitive bot for testing

---
+++ src/bots/bull_team.h
@@ -0,0 +1,15 @@
+#ifndef GAME_THEORY_BULL_TEAM_H
+#define GAME_THEORY_BULL_TEAM_H
+
+#include <iostream>
+#include "../team.h"
+
+class BullTeam : public Team {
+public:
+    explicit BullTeam(int _id);
+    void set_prod(int val);
+    std::shared_ptr<ITurnAction> make_turn(TurnData match);
+    ~BullTeam();
+};
+
+#endif //GAME_THEORY_BULL_TEAM_H
