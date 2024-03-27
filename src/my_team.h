750e36ee0b0e00797d6bd05e540a6958717f898f
IlyaChichkov <ilya555chich@yandex.ru>
2024-03-27 10:13:14 +0300

    feat: move get_rand to Utils class & make static

--- src/my_team.h
+++ src/my_team.h
@@ -2,13 +2,13 @@
 #define GAME_THEORY_MY_TEAM_H
 
 #include <iostream>
+#include "utils.h"
 #include "team.h"
 
 class MyTeam : public Team {
 public:
     explicit MyTeam(int _id);
     explicit MyTeam(int _id, std::string _name);
-    int get_rand(int from, int to);
     std::shared_ptr<ITurnAction> make_turn(TurnData match);
     ~MyTeam();
 };
