afd438adca42b723a34387492bb8b72bcdc14eb7
IlyaChichkov <ilya555chich@yandex.ru>
2024-03-27 10:13:14 +0300

    feat: move get_rand to Utils class & make static

--- src/my_team.cpp
+++ src/my_team.cpp
@@ -1,9 +1,5 @@
 #include "my_team.h"
 
-int MyTeam::get_rand(int from, int to) {
-    return std::rand() % (to - from) + from;
-}
-
 std::shared_ptr<ITurnAction> MyTeam::make_turn(TurnData data) {
     // auto inc = get_increase_production_actions();
     // std::cout << "get_increase_production_actions:" << inc.size() << std::endl;
@@ -14,7 +10,7 @@ std::shared_ptr<ITurnAction> MyTeam::make_turn(TurnData data) {
     // auto str = get_strike_actions();
     // std::cout << "get_strike_actions:" << str.size() << std::endl;
 
-    std::shared_ptr<ITurnAction> action = turn_actions.at(get_rand(0, turn_actions.size()));
+    std::shared_ptr<ITurnAction> action = turn_actions.at(Utils::get_rand_int(0, turn_actions.size()));
 
     switch (action->actionType) {
         case TurnActionType::None:
@@ -25,7 +21,7 @@ std::shared_ptr<ITurnAction> MyTeam::make_turn(TurnData data) {
         case TurnActionType::Strike:
             std::shared_ptr<ProvokeStrike> strike = std::dynamic_pointer_cast<ProvokeStrike>(action);
             auto opponents = data.get_opponents(id);
-            strike->set_target(opponents.at(get_rand(0, opponents.size())));
+            strike->set_target(opponents.at(Utils::get_rand_int(0, opponents.size())));
             break;
     }
 
---
+++ src/utils.h
@@ -0,0 +1,11 @@
+#ifndef GAME_THEORY_UTILS_H
+#define GAME_THEORY_UTILS_H
+
+#include <iostream>
+
+class Utils {
+public:
+    static int get_rand_int(int from, int to);
+};
+
+#endif //GAME_THEORY_UTILS_H
