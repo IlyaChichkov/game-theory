1393c3758e28316621c071a68ddd1b428d912052
IlyaChichkov <ilya555chich@yandex.ru>
2024-03-28 14:03:38 +0300

    feat: add match configuration console print before start

--- src/game_match.h
+++ src/game_match.h
@@ -51,6 +51,7 @@ private:
     void setup_teams();
     void complete_turn();
     int get_income(int production);
+    void print_match_configuration();
     void print_turn_results();
     void print_match_results();
     void compute_turn_results();
